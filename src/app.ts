f73f7e18349b616facb7bfb19bf43b127cafb1c2
Animesh-03 <anivad03@gmail.com>
2023-02-08 02:09:31 +0530

    initial commit
    Added TS, a basic express server and docker-compose

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { PORT } from './constants';
+
+
+const app = express();
+app.use(express.json());
+
+
+app.get("/", (req: express.Request, res:express.Response) => {
+    req.body;
+    res.send({"msg": "Hello World!"});
+});
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
