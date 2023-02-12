6660cd704f60e2136229e2bc9ef43d84e68e89f7
Animesh-03 <anivad03@gmail.com>
2023-02-12 15:11:10 +0530

    add mongo and redis containers

--- src/app.ts
+++ src/app.ts
@@ -11,4 +11,4 @@ app.get("/", (req: express.Request, res:express.Response) => {
     res.send({"msg": "Hello World!"});
 });
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
